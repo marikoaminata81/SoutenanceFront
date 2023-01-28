582d20cc8c484393af024dc8ac905865fbb2489c
marikoaminata81 <107038393+marikoaminata81@users.noreply.github.com>
2023-01-28 19:58:55 +0000

    commit6

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tab5',
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
 })
-export class Tab5Page implements OnInit {
+export class Tab5Page {
+  selectedSegment: string='video';
 
+ 
   constructor() { }
 
-  ngOnInit() {
+  segmentChanged(ev:any){
+    console.log(ev.target.value);
+    this.selectedSegment=ev.target.value;
   }
+
+
+
 
 }
